be6de5af6bdc32ffe71ff5ea3586b558dc874b59
bryan-philips <philipus.bryanrahail@gmail.com>
2024-03-08 17:20:00 +0800

    menambahakan code pada file index.html berupa script yang akan terhubung ke file script.js. serta menambhkan code toggle pada file script.js untuk menu hamburger dapat di klik on/off

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Toggle Class Active 
+const navbarNav = document.querySelector('.nav-nav');
+
+// ketika ham-menu di klik 
+document.querySelector('#ham-menu').onclick = () => {
+    navbarNav.classList.toggle('active')
+};
