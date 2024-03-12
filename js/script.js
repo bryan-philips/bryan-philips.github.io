66d18fdaac232bcfc45e3fefeebf5f3d7ff5cd39
bryan-philips <philipus.bryanrahail@gmail.com>
2024-03-12 08:03:12 +0800

    menambah style about pada file style.css dan mengubah code skill, experience, education pada bagian about file index.html

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ const navbarNav = document.querySelector('.nav-nav');
 document.querySelector('#ham-menu').onclick = () => {
     navbarNav.classList.toggle('active')
 };
+
+
+// bagian About 
+
+const tlinks = document.getElementsByClassName('tlinks');
+const tcontents = document.getElementsByName('tcontents');
+
+function taBuka(Tbaru){
+    for(tlink of tlinks){
+        tlink.classList.remove("active-link");
+    }
+
+    for(tcontent of tcontents){
+        tcontent.classList.remove("active-content");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(Tbaru).classList.add("active-content")
+}
+
